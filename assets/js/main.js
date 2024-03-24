92f9021e9c14b94e27fb6024250e1d5874ef2c2d
Yaasir <muhammad.hosseny@umail.uom.ac.mu>
2024-03-24 19:51:59 +0400

    4eme commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: UpConstruction
-* Updated: Jan 29 2024 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
